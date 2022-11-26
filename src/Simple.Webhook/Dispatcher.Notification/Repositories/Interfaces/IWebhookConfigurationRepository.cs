2cce5871a4496a4cb919245c946bebf042b2ffce
Igor Walace Cuevas <igor.w.cuevas@gmail.com>
2022-11-26 14:32:06 -0300

    feat: implement repository redis

---
+++ src/Simple.Webhook/Dispatcher.Notification/Repositories/Interfaces/IWebhookConfigurationRepository.cs
@@ -0,0 +1,7 @@
+﻿using Simple.Webhook.Shared;
+
+namespace Dispatcher.Notification.Repositories.Interfaces;
+public interface IWebhookConfigurationRepository
+{
+    Task<IEnumerable<WebhookConfiguration>?> GetWebhookConfigurationsAsync(string eventName);
+}
---
+++ src/Simple.Webhook/Dispatcher.Notification/Repositories/WebhookConfigurationRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dispatcher.Notification.Repositories.Interfaces;
+using Microsoft.Extensions.Caching.Distributed;
+using Simple.Webhook.Shared;
+using System.Text.Json;
+
+namespace Dispatcher.Notification.Repositories;
+
+public class WebhookConfigurationRepository : IWebhookConfigurationRepository
+{
+    private readonly IDistributedCache distributedCache;
+
+    public WebhookConfigurationRepository(IDistributedCache distributedCache)
+    {
+        this.distributedCache = distributedCache ?? throw new ArgumentNullException(nameof(distributedCache));
+    }
+
+    public async Task<IEnumerable<WebhookConfiguration>?> GetWebhookConfigurationsAsync(string eventName)
+    {
+        var configurationsJson = await distributedCache.GetStringAsync(eventName);
+        if (string.IsNullOrEmpty(configurationsJson))
+            return null;
+        return JsonSerializer.Deserialize<List<WebhookConfiguration>>(configurationsJson);
+    }
+}
